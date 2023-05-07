50b6182b34f53a09a84bcdc37242776d3d2f053a
Rushi Padhiyar <86287364+cyph3rryx@users.noreply.github.com>
2023-05-07 16:44:54 +0530

    Update Password Generator.py
    This code first defines three functions: generate_password, encrypt_password, and decrypt_password.
    
    The generate_password function generates a random password of the specified length. The encrypt_password function encrypts the password using the specified key and returns the encrypted password. The decrypt_password function reads the encrypted password and key from the file, decrypts the password using the key, and returns the decrypted password.
    
    The write_to_file function writes the encrypted password and key to a file. It first encodes the encrypted password and key using base64 encoding and writes them to the file in binary mode. The base64.b64encode function is used to encode the bytes objects without adding any extra characters.
    
    In the if __name__ == '__main__': block, the code generates a random password, generates a random key using Fernet, encrypts the password using the key, writes the encrypted password and key to a file, and finally decrypts the password from the file using the key and prints it to the console.
    
    Note that the key is generated randomly each time the program is run. If you want to use the same key to decrypt the password as the one used to encrypt it, you should store the key securely and load it from the file when decrypting the password.

--- Password Generator.py
+++ Password Generator.py
@@ -4,35 +4,52 @@ import base64
 from cryptography.fernet import Fernet
 
 def generate_password(length):
-  # Generate a random password
-  alphabet = string.ascii_letters + string.digits
-  password = ''.join(secrets.choice(alphabet) for i in range(length))
-  return password
+    # Generate a random password
+    alphabet = string.ascii_letters + string.digits
+    password = ''.join(secrets.choice(alphabet) for i in range(length))
+    return password
 
-def encrypt_password(password):
-  # Generate a random key
-  key = Fernet.generate_key()
+def encrypt_password(password, key):
+    # Encrypt the password using the key
+    f = Fernet(key)
+    encrypted_password = f.encrypt(password.encode())
 
-  # Encrypt the password using the key
-  f = Fernet(key)
-  encrypted_password = f.encrypt(password.encode())
-
-  # Return the encrypted password and the key
-  return encrypted_password, key
+    # Return the encrypted password
+    return encrypted_password
 
 def write_to_file(encrypted_password, key):
-  # Write the encrypted password and key to a file
-  with open('password.txt', 'w') as f:
-    f.write(base64.encodebytes(encrypted_password).decode())
-    f.write('\n')
-    f.write(base64.encodebytes(key).decode())
+    # Write the encrypted password and key to a file
+    with open('password.txt', 'wb') as f:
+        f.write(base64.b64encode(encrypted_password))
+        f.write(b'\n')
+        f.write(base64.b64encode(key))
 
-def decrypt_password(encrypted_password, key):
-  f = Fernet(key)
-  decrypted_password = f.decrypt(encrypted_password).decode()
-  return decrypted_password
+def decrypt_password(key):
+    # Read the encrypted password and key from the file
+    with open('password.txt', 'rb') as f:
+        encrypted_password = base64.b64decode(f.readline())
+        key = base64.b64decode(f.readline())
+
+    # Decrypt the password using the key
+    f = Fernet(key)
+    decrypted_password = f.decrypt(encrypted_password).decode()
+
+    # Return the decrypted password
+    return decrypted_password
 
 if __name__ == '__main__':
-  password = generate_password(30)
-  encrypted_password, key = encrypt_password(password)
-  write_to_file(encrypted_password, key)
+    # Generate a random password
+    password = generate_password(30)
+
+    # Generate a random key
+    key = Fernet.generate_key()
+
+    # Encrypt the password using the key
+    encrypted_password = encrypt_password(password, key)
+
+    # Write the encrypted password and key to a file
+    write_to_file(encrypted_password, key)
+
+    # Decrypt the password from the file
+    decrypted_password = decrypt_password(key)
+    print(decrypted_password)
