875d2fdc10ae1752343a0070fb12926434dd3218
Rushi Padhiyar <86287364+cyph3rryx@users.noreply.github.com>
2023-05-11 16:05:41 +0530

    Delete Decrypter.py

--- Decrypter.py
+++
@@ -1,5 +0,0 @@
-with open('password.txt', 'r') as f:
-  encrypted_password = base64.decodebytes(f.readline().strip().encode())
-  key = base64.decodebytes(f.readline().strip().encode())
-  decrypted_password = decrypt_password(encrypted_password, key)
-  print(decrypted_password)
